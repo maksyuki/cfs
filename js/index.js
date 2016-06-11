f30e46e01404a96689c3162f82807584f30a24f6
maksyuki <myc20404401@sina.com>
2016-06-11 23:04:45 +0800

    Add some features
    1.Initialize the webpages

---
+++ js/index.js
@@ -0,0 +1,23 @@
+window.onload = banner;
+
+function banner() {
+    picture = $(".banner_picture");
+    picture_length = picture.length;
+    cur_picture = 0;
+    $(picture[cur_picture]).css({"opacity": "1", "z-index": "1"});
+    $("#banner .left").click(moveLeft);
+    $("#banner .right").click(moveRight);
+    setInterval(moveLeft, 8000);
+}
+
+function moveLeft() {
+    $(picture[cur_picture]).css({"opacity": "0", "z-index": "0"});
+    cur_picture == picture_length - 1? cur_picture = 0 : cur_picture++;
+    $(picture[cur_picture]).css({"opacity": "1", "z-index": "1"});
+}
+
+function moveRight() {
+    $(picture[cur_picture]).css({"opacity": "0", "z-index": "0"});   
+    cur_picture == 0 ? cur_picture = picture_length - 1 : cur_picture--;
+    $(picture[cur_picture]).css({"opacity": "1", "z-index": "1"});
+}
