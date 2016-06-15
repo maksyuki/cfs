6e18b5de4039f5db66a8d7cc6cb3e3c65f5a65ae
maksyuki <myc20404401@sina.com>
2016-06-15 20:40:40 +0800

    Add banner button
    1.add action on the banner button.
    2.modify css on the banner button.

--- js/index.js
+++ js/index.js
@@ -4,20 +4,32 @@ function banner() {
     picture = $(".banner_picture");
     picture_length = picture.length;
     cur_picture = 0;
+    point_nav = $(".circle");
     $(picture[cur_picture]).css({"opacity": "1", "z-index": "1"});
+    $(point_nav[cur_picture]).css({"background": "white"});
     $("#banner .left").click(moveLeft);
     $("#banner .right").click(moveRight);
-    setInterval(moveLeft, 8000);
+    move_state = setInterval(moveRight, 8000);
+    $(".left, .right").hover(function () {
+        clearInterval(move_state);
+    }, function () {
+        if (move_state) clearInterval(move_state);
+        move_state = setInterval(moveRight, 8000);
+    });
 }
 
 function moveLeft() {
     $(picture[cur_picture]).css({"opacity": "0", "z-index": "0"});
-    cur_picture == picture_length - 1 ? cur_picture = 0 : cur_picture++;
+    $(point_nav[cur_picture]).css({"background": ""});
+    cur_picture == 0 ? cur_picture = picture_length - 1 : cur_picture--;
     $(picture[cur_picture]).css({"opacity": "1", "z-index": "1"});
+    $(point_nav[cur_picture]).css({"background": "white"});
 }
 
 function moveRight() {
     $(picture[cur_picture]).css({"opacity": "0", "z-index": "0"});
-    cur_picture == 0 ? cur_picture = picture_length - 1 : cur_picture--;
+    $(point_nav[cur_picture]).css({"background": ""});
+    cur_picture == picture_length - 1 ? cur_picture = 0 : cur_picture++;
     $(picture[cur_picture]).css({"opacity": "1", "z-index": "1"});
+    $(point_nav[cur_picture]).css({"background": "white"});
 }
