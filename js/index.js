ce072a4f3ea6aee1ef788d955a2e19093afa4be4
maksyuki <myc20404401@sina.com>
2016-07-14 09:56:30 +0800

    Add the dynamic "banner_text" in the banner
    1.add the dynamic "banner_text" in the banner.
    2.modify the index.js.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 // addLoadEvent(showNav);
-addLoadEvent(banner);
+// addLoadEvent(showBanner);
 addLoadEvent(showMedia);
+$(document).ready(showBanner);
 $(document).ready(showNav);
 $(document).ready(showfullPage);
+$(document).ready(function () {
+    $(".banner_zero").css({"opacity": "1", "top": "62%"});
+});
 
 function addLoadEvent(func) {
     var oldonload = window.onload;
@@ -45,61 +49,66 @@ function showNav() {
     });
 }
 
-function banner() {
-    picture = $(".banner_picture");
-    point_nav = $(".circle");
-    banner_text = $(".banner_text");
-    picture_length = picture.length;
-    cur_picture = 0;
-    $(picture[cur_picture]).css({"opacity": "1", "z-index": "1"});
-    $(point_nav[cur_picture]).css({"background": "white"});
-    $(banner_text[cur_picture]).css({"opacity": "1", "top": "62%"});
-    $("#banner .left").click(moveLeft);
-    $("#banner .right").click(moveRight);
-    move_state = setInterval(moveRight, 8000);
-    $(".left, .right").hover(function () {
-        clearInterval(move_state);
-    }, function () {
-        if (move_state) clearInterval(move_state);
-        move_state = setInterval(moveRight, 8000);
-    });
-    setInterval(moveDown, 2000);
-}
-
-function moveLeft() {
-    $(picture[cur_picture]).css({"opacity": "0", "z-index": "0"});
-    $(point_nav[cur_picture]).css({"background": ""});
-    $(banner_text[cur_picture]).css({"transition": "opacity, top, 0.2s linear"});
-    $(banner_text[cur_picture]).css({"opacity": "0", "top": "65%"});
-    cur_picture == 0 ? cur_picture = picture_length - 1 : cur_picture--;
-    $(picture[cur_picture]).css({"opacity": "1", "z-index": "1"});
-    $(point_nav[cur_picture]).css({"background": "white"});
-    $(banner_text[cur_picture]).css({"transition": "opacity, top, 0.3s linear 1s"});
-    $(banner_text[cur_picture]).css({"opacity": "1", "top": "62%"});
-}
-
-function moveRight() {
-    $(picture[cur_picture]).css({"opacity": "0", "z-index": "0"});
-    $(point_nav[cur_picture]).css({"background": ""});
-    $(banner_text[cur_picture]).css({"transition": "opacity, top, 0.2s linear"});
-    $(banner_text[cur_picture]).css({"opacity": "0", "top": "65%"});
-    cur_picture == picture_length - 1 ? cur_picture = 0 : cur_picture++;
-    $(picture[cur_picture]).css({"opacity": "1", "z-index": "1"});
-    $(point_nav[cur_picture]).css({"background": "white"});
-    $(banner_text[cur_picture]).css({"transition": "opacity, top, 0.3s linear 1s"});
-    $(banner_text[cur_picture]).css({"opacity": "1", "top": "62%"});
-}
-
-function moveDown() {
-    var arrow_1 = $("#arrow1");
-    var arrow_2 = $("#arrow2");
-    if (arrow_1.css("opacity") == "0") {
-        arrow_1.css({"opacity": "1"});
-        arrow_2.css({"opacity": "1"});
-    } else {
-        arrow_1.css({"opacity": "0"});
-        arrow_2.css({"opacity": "0"});
-    }
+// function showBanner() {
+//     picture = $(".banner_picture");
+//     point_nav = $(".circle");
+//     banner_text = $(".banner_text");
+//     picture_length = picture.length;
+//     cur_picture = 0;
+//     $(picture[cur_picture]).css({"opacity": "1", "z-index": "1"});
+//     $(point_nav[cur_picture]).css({"background": "white"});
+//     $(banner_text[cur_picture]).css({"opacity": "1", "top": "62%"});
+//     $("#banner .left").click(moveLeft);
+//     $("#banner .right").click(moveRight);
+//     move_state = setInterval(moveRight, 8000);
+//     $(".left, .right").hover(function () {
+//         clearInterval(move_state);
+//     }, function () {
+//         if (move_state) clearInterval(move_state);
+//         move_state = setInterval(moveRight, 8000);
+//     });
+//     setInterval(moveDown, 2000);
+// }
+//
+// function moveLeft() {
+//     $(picture[cur_picture]).css({"opacity": "0", "z-index": "0"});
+//     $(point_nav[cur_picture]).css({"background": ""});
+//     $(banner_text[cur_picture]).css({"transition": "opacity, top, 0.2s linear"});
+//     $(banner_text[cur_picture]).css({"opacity": "0", "top": "65%"});
+//     cur_picture == 0 ? cur_picture = picture_length - 1 : cur_picture--;
+//     $(picture[cur_picture]).css({"opacity": "1", "z-index": "1"});
+//     $(point_nav[cur_picture]).css({"background": "white"});
+//     $(banner_text[cur_picture]).css({"transition": "opacity, top, 0.3s linear 1s"});
+//     $(banner_text[cur_picture]).css({"opacity": "1", "top": "62%"});
+// }
+//
+// function moveRight() {
+//     $(picture[cur_picture]).css({"opacity": "0", "z-index": "0"});
+//     $(point_nav[cur_picture]).css({"background": ""});
+//     $(banner_text[cur_picture]).css({"transition": "opacity, top, 0.2s linear"});
+//     $(banner_text[cur_picture]).css({"opacity": "0", "top": "65%"});
+//     cur_picture == picture_length - 1 ? cur_picture = 0 : cur_picture++;
+//     $(picture[cur_picture]).css({"opacity": "1", "z-index": "1"});
+//     $(point_nav[cur_picture]).css({"background": "white"});
+//     $(banner_text[cur_picture]).css({"transition": "opacity, top, 0.3s linear 1s"});
+//     $(banner_text[cur_picture]).css({"opacity": "1", "top": "62%"});
+// }
+//
+// function moveDown() {
+//     var arrow_1 = $("#arrow1");
+//     var arrow_2 = $("#arrow2");
+//     if (arrow_1.css("opacity") == "0") {
+//         arrow_1.css({"opacity": "1"});
+//         arrow_2.css({"opacity": "1"});
+//     } else {
+//         arrow_1.css({"opacity": "0"});
+//         arrow_2.css({"opacity": "0"});
+//     }
+// }
+function showBanner() {
+    setInterval(function () {
+        $.fn.fullpage.moveSlideRight();
+    }, 8000);
 }
 
 function showMedia() {
@@ -126,9 +135,13 @@ function showfullPage() {
         showActiveTooltip: true,
         navigation: true,
         controlArrows: false,
-        easing: "easeInQuart"
+        easing: "easeInQuart",
+        onSlideLeave: function (anchorLink, index, slideIndex, direction, nextSlideIndex) {
+            banner_text = $(".banner_text");
+            $(banner_text[slideIndex]).css({"transition": "opacity, top, 0.2s linear"});
+            $(banner_text[slideIndex]).css({"opacity": "0", "top": "65%"});
+            $(banner_text[nextSlideIndex]).css({"transition": "opacity, top, 0.3s linear 1s"});
+            $(banner_text[nextSlideIndex]).css({"opacity": "1", "top": "62%"});
+        }
     });
-    setInterval(function () {
-        $.fn.fullpage.moveSlideRight();
-    }, 8000);
 }
